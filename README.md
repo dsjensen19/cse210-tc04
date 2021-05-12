726f32e2f64f73622f626f77d5bc5957948bb5df
Sombreroboy <83668223+Sombreroboy@users.noreply.github.com>
2021-05-12 12:26:31 -0600

    add dealer base

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
+<<<<<<< Updated upstream
 # cse210-tc04
+=======
+Dealer function:
+start_game(){
+
+}
+#1st card  =get_card() {
+    return int
+#call player.higher/lower
+#2nd card =get_card()
+#def compare_card()
+call player.keep_playing
+}
+can_deal(bool){
+
+}
+ 
+Var: card, continue
+
+
+
+
+Player function:
+higher_lower {
+    return bool
+}
+print_score(int) {
+return score
+}
+keep_playing(string) {
+    return bool
+}
+Var: score
+
+>>>>>>> Stashed changes
---
+++ game/dealer.py
@@ -0,0 +1,89 @@
+from game.player import player
+import random
+
+class Dealer:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+        card_1 (int): Whether or not the player wants to keep playing.
+        card_2 (int): The total number of points earned.
+        can_has_player (bool)
+        can_deal (bool): An instance of the class of objects known as player.
+    """
+
+
+
+    def start_game(self):
+
+        pass
+
+    def get_card(self,):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means throwing the dice.
+
+        Args:
+            self (Dealer): An instance of Dealer.
+        """
+
+        card = random.randint(1,13)
+        return card
+
+    def display_card(self, card, card_num):
+        if card_num == 1:
+            card_num_str = "first"
+        else:
+            card_num_str = "Second"
+        card_str = card.String()
+        if card == 11:
+            card_str == "Jack"
+        if card == 12:
+            card_str == "Queen"
+        if card == 13:
+            card_str == "King"
+        if card == 1:
+            card_str == "Ace"
+
+        print(f"The {card_num_str} card is a {card_str}.")
+        
+    def get_guess(self):
+        """gets input from player
+        Args:
+            self (Dealer): An instance of Dealer.
+        """
+        guess = self.player.higher_or_lower
+
+    def compare_cards(card_1,card_2, guess):
+        #guess is boolean, if input = lower then Guess = False
+        # if input = Higher then guess = True
+        """
+        Compares cards to determine higher_lower, 
+        compares result with guess
+        Args: 
+            self: : An instance of Dealer.
+            card_1: int
+            card_2: int
+            guess: bool
+        """
+
+        if guess:
+            if card_1 == card_2:
+                print(f"{card_1} is equal to {card_2}")
+                return False
+            elif card_1 > card_2:
+                print(f"{card_1} is bigger than {card_2}")
+                return False
+            elif card_1 < card_2:
+                print(f"{card_2} is bigger than {card_1}")
+                return True
+        if not guess:
+            if card_1 == card_2:
+                print(f"{card_1} is equal to {card_2}")
+                return False
+            elif card_1 > card_2:
+                print(f"{card_1} is bigger than {card_2}")
+                return True
+            elif card_1 < card_2:
+                print(f"{card_2} is bigger than {card_1}")
+                return False
