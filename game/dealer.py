1f5203acd2f8a372b6063eccf7b27090559ec8a1
Seth Burton <sethburton80@gmail.com>
2021-05-12 19:39:45 -0600

    Tyson And Seths Finished Work

--- game/dealer.py
+++ game/dealer.py
@@ -22,8 +22,15 @@ class Dealer:
         self.can_deal = True
 
         
-
+    
     def start_game(self):
+        """
+        runs the take turn function untill the game is over
+        """
+        while self.can_deal:
+            self.take_turn()
+
+    def take_turn(self):
         """ runs the main functions of the game in order
         1) draw first card
         2) show first card
@@ -31,75 +38,69 @@ class Dealer:
         4) draw second card
         5) show second card 
         6) compare cards
-        7) calculate score change and send to player
+        7) calculate score change and send to playerf
         8) has the player print their score
         9) ask player if they want to play again
         """
-        while self.can_deal:
-            self.card_1 = self.get_card()
-            self.display_card_1()
-            self.guess = self.player.higher_lower()
-            self.card_2 = self.get_card()
-            self.display_card_2()
-            
-            #
-            #note should not need score change.
-            #
-            score_change = self.compare_cards(self.card_1, self.card_2, self.guess)
-            if score_change:
-                self.player.score += 100
-            else:
-                self.player.score -= 75
-            self.player.print_score()
-            if self.player.score > 0:
-                self.can_deal  = self.player.keep_playing()
-            else:
-                self.can_deal = False
+    
+        self.card_1 = self.get_card()
+        self.display_card_1()
+        guess = self.player.higher_lower()
+        self.card_2 = self.get_card()
+        self.display_card_2()
+        self.compare_cards(guess)
+        self.player.print_score()
+        if self.player.score > 0:
+            self.can_deal = self.player.keep_playing()
             print("\n")
+        else:
+            self.can_deal = False
+            print("Game overThanks for playing!")
 
     def get_card(self):
-        """Gets the inputs at the beginning of each round of play. In this case,
-        that means throwing the dice.
+        """Generates a random card out of a deck.
 
         Args:
             self (Dealer): An instance of Dealer.
         """
+
         card = random.randint(1,13)
         return card
 
     #i made this 
     """streamline the printing of card 1"""
     def display_card_1(self):
-        print(f"The card is: {self.card_1}") 
+        print(f"Your first card is {self.get_card_str(self.card_1)}")
     #i made this
     """streamline the printing of card 2"""
     def display_card_2(self):
-        print(f"Next card was: {self.card_2}") 
+        print(f"Your first card is {self.get_card_str(self.card_2)}") 
 
     #
     #note
     #I want to change this to be a little simpler
     #by not having inputs or outputs
     #
-    def display_card(self, card, card_num):
-        if card_num == 1:
-            card_num_str = "first"
-        else:
-            card_num_str = "Second"
-        #
-        #changed card.String() to str(card)
-        #
+    def get_card_str(self, card):
+
+        """
+        gets the string for the card number
+        Ace, 2-10, Jack, Queen, King
+        """
         card_str = str(card)
         if card == 11:
-            card_str == "Jack"
+            card_str = "Jack"
         if card == 12:
-            card_str == "Queen"
+            card_str = "Queen"
         if card == 13:
-            card_str == "King"
+            card_str = "King"
         if card == 1:
-            card_str == "Ace"
+            card_str = "Ace"
+            
+        return card_str
 
-        print(f"The {card_num_str} card is a {card_str}.")
+
+        
        
     def get_guess(self):
         """gets input from player
@@ -107,13 +108,14 @@ class Dealer:
             self (Dealer): An instance of Dealer.
         """
         guess = self.player.higher_or_lower
+
     #
     #note
     #I want to change this to be mor simple
     #by not having inputs 
     # and instead of outputs just change the players score.
     #
-    def compare_cards(self, card_1, card_2, guess):
+    def compare_cards(self, guess):
         """guess is boolean, if input = lower then Guess = False
         if input = Higher then guess = True
         """
@@ -123,27 +125,29 @@ class Dealer:
         compares result with guess
         Args: 
             self: : An instance of Dealer.
-            card_1: int
-            card_2: int
+            self.card_1: int
+            self.card_2: int
             guess: bool
         """
-        if guess:
-            if card_1 == card_2:
-                #print(f"{card_1} is equal to {card_2}")
-                return False
-            elif card_1 > card_2:
-                #print(f"{card_1} is bigger than {card_2}")
-                return False
-            elif card_1 < card_2:
-                #print(f"{card_2} is bigger than {card_1}")
-                return True
+        card_str_1 = self.get_card_str(self.card_1)
+        card_str_2 = self.get_card_str(self.card_2)
+        if guess: 
+            if self.card_1 == self.card_2:
+                print(f"{card_str_1} is equal to {card_str_2}")
+                self.player.score -= 75
+            elif self.card_1 > self.card_2:
+                print(f"{card_str_1} is higher than {card_str_2}")
+                self.player.score -= 75
+            elif self.card_1 < self.card_2:
+                print(f"{card_str_1} is lower than {card_str_2}")
+                self.player.score += 100
         if not guess:
-            if card_1 == card_2:
-                #print(f"{card_1} is equal to {card_2}")
-                return False
-            elif card_1 > card_2:
-                #print(f"{card_1} is bigger than {card_2}")
-                return True
-            elif card_1 < card_2:
-                #print(f"{card_2} is bigger than {card_1}")
-                return False
+            if self.card_1 == self.card_2:
+                print(f"{card_str_1} is equal to {card_str_2}")
+                self.player.score -= 75
+            elif self.card_1 > self.card_2:
+                print(f"{card_str_1} is higher than {card_str_2}")
+                self.player.score += 100
+            elif self.card_1 < self.card_2:
+                print(f"{card_str_1} is lower than {card_str_2}")
+                self.player.score -= 75
