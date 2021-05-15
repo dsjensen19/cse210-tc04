232025dad82d9f4dffa9b35dc778a42b9af4c682
dsjensen19 <83721681+dsjensen19@users.noreply.github.com>
2021-05-15 16:37:20 -0600

    Update player.py

--- game/player.py
+++ game/player.py
@@ -1,9 +1,28 @@
 class Player():
+    """A code template for the player of the game. Decisons are made by whoever is playing the game.
+    """
     def __init__(self):
         self.score = 300
+        self.director = Director()
     def keep_playing(self):
         return False
+        
+        
     def higher_lower(self):
+        """This program recieves input from the user of their guess and returns True is their guess is 
+        higher and False if their guess is lower"""
+        while True:
+            guess = str(input("Higher or Lower?" [h/l]"))
+            if (guess == "h"):
+                return True
+            elif (guess == "l"):
+                return False
+            else:
+                print("Please enter a valid guess")
+                
         return True
-    def print_score(self):
-        print(self.score)
+        
+    def print_score(self, score):
+        """This program prints the score to the user
+        """
+        print(f"Your score is {self.score}")
